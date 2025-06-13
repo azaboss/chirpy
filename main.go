d44120926a4d53f99b05950349a5a8d46391906c
Azamat Karabayev <aza@Mac-mini-Azamat.local>
2025-06-14 00:37:17 +0500

    add mux.HandleFunc

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func main() {
 		Handler: mux,
 	}
 	fmt.Println("Starting server on :8080")
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	hh := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
+	mux.HandleFunc("/healthz", hh)
 	s.ListenAndServe()
 }
